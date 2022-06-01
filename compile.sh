7901d78bd0cd33973bf2176b991046d67e27c03f
tolya-yanot <1449561+tolya-yanot@users.noreply.github.com>
2022-06-02 01:52:53 +0300

    use consts for errors and ops

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-func -SPA -o ./build/jetton-wallet.fif stdlib.fc params.fc op-codes.fc jetton-utils.fc jetton-wallet.fc
-func -SPA -o ./build/jetton-minter.fif stdlib.fc params.fc op-codes.fc jetton-utils.fc jetton-minter.fc
+func -SPA -o ./build/jetton-wallet.fif stdlib.fc op-codes.fc params.fc jetton-utils.fc jetton-wallet.fc
+func -SPA -o ./build/jetton-minter.fif stdlib.fc op-codes.fc params.fc jetton-utils.fc jetton-minter.fc
 
 fift -s build/print-hex.fif
