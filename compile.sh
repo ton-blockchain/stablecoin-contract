3c50d72babc21b07dd43417d1cd57adb3ea852e6
dariotarantini <dariotarantinimail@gmail.com>
2022-06-02 22:40:15 +0200

    added constants to the code

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-func -SPA -o ./build/jetton-wallet.fif stdlib.fc op-codes.fc params.fc jetton-utils.fc jetton-wallet.fc
-func -SPA -o ./build/jetton-minter.fif stdlib.fc op-codes.fc params.fc jetton-utils.fc jetton-minter.fc
+func -SPA -o ./build/jetton-wallet.fif contracts/jetton-wallet.fc
+func -SPA -o ./build/jetton-minter.fif contracts/jetton-minter.fc
 
 fift -s build/print-hex.fif
