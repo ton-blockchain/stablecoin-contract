e5563bbc865a893f5b3df20c692312e597c7f917
Trinketer22 <trinketer22@localhost>
2023-08-28 23:03:23 +0300

    Cosmetic

--- wrappers/JettonWallet.ts
+++ wrappers/JettonWallet.ts
@@ -58,8 +58,10 @@ export class JettonWallet implements Contract {
                            customPayload: Cell | null,
                            forward_ton_amount: bigint,
                            forwardPayload: Cell | null) {
+
         return beginCell().storeUint(Op.transfer, 32).storeUint(0, 64) // op, queryId
-                          .storeCoins(jetton_amount).storeAddress(to)
+                          .storeCoins(jetton_amount)
+                          .storeAddress(to)
                           .storeAddress(responseAddress)
                           .storeMaybeRef(customPayload)
                           .storeCoins(forward_ton_amount)
