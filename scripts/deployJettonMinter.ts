bd09994481f0df39bb665dd8b2d4db60c40cbaf1
Trinketer22 <trinketer22@localhost>
2023-08-31 15:38:22 +0300

    Simple deploy script

---
+++ scripts/deployJettonMinter.ts
@@ -0,0 +1,30 @@
+import { Address, toNano } from '@ton/core';
+import { JettonMinter } from '../wrappers/JettonMinter';
+import { compile, NetworkProvider, UIProvider} from '@ton-community/blueprint';
+import { promptAddress, promptBool, promptUrl } from '../wrappers/ui-utils';
+
+
+export async function run(provider: NetworkProvider) {
+    const ui       = provider.ui();
+    const sender   = provider.sender();
+    const adminPrompt = `Please specify admin address`;
+
+    let admin: Address;
+    let dataCorrect: boolean;
+
+    do {
+        admin  = await promptAddress(adminPrompt, ui, sender.address);
+        ui.write("Please verify data:\n")
+        ui.write(`Admin:${admin}\n\n`);
+        dataCorrect = await promptBool('Is everything ok?(y/n)', ['y','n'], ui);
+    } while(!dataCorrect);
+
+    const wallet_code = await compile('JettonWallet');
+
+    const minter  = JettonMinter.createFromConfig({admin,
+                                                  wallet_code,
+                                                  }, 
+                                                  await compile('JettonMinter'));
+
+    await provider.deploy(minter, toNano('0.05'));
+}
