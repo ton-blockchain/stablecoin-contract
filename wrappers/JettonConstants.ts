c9038551ed5d60e7f6619bc7fee507c84ad32f5b
Behrang Norouzinia <behrangn@gmail.com>
2024-03-29 17:30:49 +0330

    Add missing query_id field to TLB schema

--- wrappers/JettonConstants.ts
+++ wrappers/JettonConstants.ts
@@ -5,16 +5,16 @@ export abstract class Op {
     static excesses = 0xd53276db;
     static burn = 0x595f07bc;
     static burn_notification = 0x7bdd97de;
-    
+
     static provide_wallet_address = 0x2c76b973;
     static take_wallet_address = 0xd1735400;
     static mint = 0x642b7d07;
     static change_admin = 0x6501f354;
     static claim_admin = 0xfb88e119;
-    static upgrade = 0x2508d66a;
+    static upgrade = 0x2ea15808;
     static call_to = 0x235caf52;
     static top_up = 0xd372158c;
-    static change_metadata_url = 0xcb862902;
+    static change_metadata_url = 0x70e8e889;
     static set_status = 0xeed236d3;
 }
 
@@ -24,12 +24,10 @@ export abstract class Errors {
     static not_owner = 73;
     static not_valid_wallet = 74;
     static wrong_workchain = 333;
-    
+
     static contract_locked = 45;
     static balance_error = 47;
     static not_enough_gas = 48;
     static invalid_mesage = 49;
     static discovery_fee_not_matched = 75;
 }
-
-
