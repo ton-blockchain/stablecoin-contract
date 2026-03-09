fc84e5470ade1d0245c338e187e98cc3a5491ce1
cynthiaadkison1-crypto <cynthiaadkison1@gmail.com>
2026-03-08 22:48:26 -0500

    Revert "Stablecoin lib doc"

--- scripts/deployJettonMinter.ts
+++ scripts/deployJettonMinter.ts
@@ -1,8 +1,7 @@
-import {Cell, toNano} from '@ton/core';
+import {toNano} from '@ton/core';
 import {JettonMinter} from '../wrappers/JettonMinter';
 import {compile, NetworkProvider} from '@ton/blueprint';
-import {jettonWalletCodeFromLibrary, promptBool, promptUrl, promptUserFriendlyAddress} from "../wrappers/ui-utils";
-import { Librarian } from '../wrappers/Librarian';
+import {jettonWalletCodeFromLibrary, promptUrl, promptUserFriendlyAddress} from "../wrappers/ui-utils";
 
 export async function run(provider: NetworkProvider) {
     const isTestnet = provider.network() !== 'mainnet';
@@ -16,44 +15,6 @@ export async function run(provider: NetworkProvider) {
     const jettonMetadataUri = await promptUrl("Enter jetton metadata uri (https://jettonowner.com/jetton.json)", ui)
 
     const jettonWalletCode = jettonWalletCodeFromLibrary(jettonWalletCodeRaw);
-
-    const librarianCode = await compile('Librarian');
-    const librarian = provider.open(Librarian.createFromConfig({code: jettonWalletCodeRaw}, librarianCode));
-
-    const librarianState = await librarian.getState();
-
-    let librarianFound  = false;
-    let libraryDeployed = false;
-
-    let emptyCell = new Cell();
-
-    if(librarianState.state.type == 'active') {
-        librarianFound = true;
-        if(librarianState.state.code && librarianState.state.data) {
-            const codeCell = Cell.fromBoc(librarianState.state.code)[0];
-            const dataCell = Cell.fromBoc(librarianState.state.data)[0];
-            if(codeCell.equals(emptyCell) && dataCell.equals(emptyCell)) {
-                libraryDeployed = true;
-            }
-        }
-    }
-
-    if(!(librarianFound && libraryDeployed)) {
-
-        ui.write("DANGER ZONE!");
-        if(!librarianFound) {
-            ui.write("Librarian contract is not found, run deployLibrary first.")
-        } else {
-            ui.write("Librarian contract present, but it's state indicates that the library is not yet deployed.");
-            ui.write("Re-run deployLibrary but increase the TON amount or adjust librarian DEFAULT_DURATION.");
-        }
-
-        if(!await promptBool("Can you guarantee that library is already deployed?", ["Yes", "No"], ui, false)) {
-            return -1;
-        }
-    } else {
-        ui.write(`Librarian found at ${librarian.address.toString()}`);
-    }
 
     const minter = provider.open(JettonMinter.createFromConfig({
             admin: adminAddress.address,
--- scripts/deployLibrary.ts
+++ scripts/deployLibrary.ts
@@ -1,7 +1,6 @@
 import {Librarian} from '../wrappers/Librarian';
-import {compile, NetworkProvider, sleep} from '@ton/blueprint';
+import {compile, NetworkProvider} from '@ton/blueprint';
 import {promptToncoin} from "../wrappers/ui-utils";
-import { Cell } from '@ton/core';
 
 export async function run(provider: NetworkProvider) {
     const ui = provider.ui();
@@ -13,46 +12,5 @@ export async function run(provider: NetworkProvider) {
 
     const tonAmount = await promptToncoin("Enter Toncoin amount to deploy librarian. Some of Toncoins will reserved on the contract to pay storage fees. Excess will be returned.", ui);
     const librarian = provider.open(Librarian.createFromConfig({code: jettonWalletCodeRaw}, librarianCode));
-    const stateBefore = await librarian.getState();
-    const lastLt = stateBefore.last?.lt ?? 0n;
-    let emptyCell = new Cell();
-    if(stateBefore.state.type == 'active') {
-        if(stateBefore.state.code && stateBefore.state.data) {
-            const codeCell = Cell.fromBoc(stateBefore.state.code)[0];
-            const dataCell = Cell.fromBoc(stateBefore.state.data)[0];
-            if(codeCell.equals(emptyCell) && dataCell.equals(emptyCell)) {
-                ui.write("Library is already deployed, contract is bricked.");
-                return -1;
-            }
-        }
-    }
-
     await librarian.sendDeploy(provider.sender(), tonAmount);
-
-    ui.write(`Librarian at: ${librarian.address.toString()}`);
-    ui.write("Waiting for the library to deploy...");
-
-    let retryCount = 60;
-
-    do {
-        await sleep(2000);
-        const curState = await librarian.getState();
-        const curLt = curState.last?.lt ?? 0n;
-
-        if(curState.state.type == 'active' && curLt > lastLt) {
-            const codeCell = Cell.fromBoc(curState.state.code!)[0];
-            const dataCell = Cell.fromBoc(curState.state.data!)[0];
-
-            if(codeCell.equals(emptyCell) && dataCell.equals(emptyCell)) {
-                ui.write("Library deployed successfully!");
-                return 0;
-            } else {
-                ui.write("Funds were not accepted, check that amount is enough to cover the storage period in full");
-                return -1;
-            }
-        }
-    } while(retryCount--);
-
-    console.error("Transaction didn't show up on account during 2 minutes, something went wrong");
-    return -1;
 }
